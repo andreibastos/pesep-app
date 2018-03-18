0d72ae28ea125558378b18242a6ba8c17eebbd02
Andrei Bastos <andreibastos@outlook.com>
2018-03-18 15:02:19 -0300

    ajustado pasta do servidor para browser

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/server'));
+app.use(express.static(__dirname + '/dist/browser'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
