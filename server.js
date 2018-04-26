fb7228f334dda084f2d0fc91ceb9d4700c64791b
Andrei Bastos <andreibastos@outlook.com>
2018-04-25 21:30:57 -0300

    atualização de pacotes

--- server.js
+++ server.js
@@ -6,5 +6,8 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/browser'));
 
+port = process.env.PORT || 8080
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port);
+
+console.log("server start port: " +port )
