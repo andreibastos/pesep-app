7483b7f451c9797e72ecfda33a28d46504e04b58
Andrei Bastos <andreibastos@outlook.com>
2018-05-20 17:28:25 -0300

    Refatoração de tabelas de linha e de barra

--- src/app/testes-rapidos/fluxo-arquivos/fluxo-arquivos.component.ts
+++ src/app/testes-rapidos/fluxo-arquivos/fluxo-arquivos.component.ts
@@ -4,7 +4,6 @@ import { read } from 'fs';
 import { Linha } from '../../models/linha';
 import { Barra } from './../../models/barra';
 
-
 @Component({
   selector: 'app-fluxo-arquivos',
   templateUrl: './fluxo-arquivos.component.html',
@@ -12,60 +11,76 @@ import { Barra } from './../../models/barra';
 })
 export class FluxoArquivosComponent implements OnInit {
 
-  dados_carregados = [];
-  linhas: Array<Linha> = new Array();
-  barras: Array<Barra> = new Array();
+  // Arquivos realmente lidos, cada array é é um array de strings
+  arquivoLinha = [];
+  arquivoBarra = [];
+
+  linhas: Array<Linha> = new Array(); // Array de linhas (baseado no modelo)
+  barras: Array<Barra> = new Array(); // Array de barra (baseado no modelo)
+
+  // Flags para saber se os arquivos de linha e de barra foram carregados com sucesso
   linhaSucesso = false;
   barraSucesso = false;
+
+  // evento emissor. emite arquivoLinha e o arquivoBarra
   @Output() arquivosCarregados = new EventEmitter();
 
 
+  // Converte o texto separado por quebras de linha (\n) por um array [["item1", "item2", ...]]
+  static TextoParaLista(texto, tamanhoColuna) {
+    // texto: Texto a ser quebrado em array.
+    // tamanhoColuna: indica o número de colunas que deve ter cada linha.
 
-  static TextoParaLista(texto, tamanhoMinimo) {
-    const dados = [];
-    const delimitador = ',';
-    const linhas = texto.split('\n');
+    const dados = []; // dados a serem enviados para quem chama essa função
+    const delimitador = ','; // delimitador de csv (para quebrar as colunas)
+    const linhas = texto.split('\n'); // quebra o texto em linhas
 
-    linhas.forEach(linha => {
-      const colunas = linha.split(delimitador);
-      if (colunas.length === tamanhoMinimo) {
-        dados.push(colunas);
-        console.log(colunas);
+    linhas.forEach(linha => { // para cada linha em linhas
+      const colunas = linha.split(delimitador); // quebra de acordo com o delimitador
+      if (colunas.length === tamanhoColuna) { // verifica o tamanho das colunas
+        dados.push(colunas); // adiciona na lista de dados;
       }
     });
-    return dados;
+    return dados; // retorna os dados para quem o chama
   }
 
+  // contrutor vazio por enquanto.
   constructor() { }
 
+  // sobrecarga de método, padrão do angularJS.
   ngOnInit() {
   }
 
+  // Evento quando carrega novo arquivo
   carregouNovoArquivo() {
-    if (this.linhaSucesso && this.barraSucesso) {
-      this.arquivosCarregados.emit({ linhas: this.linhas, barras: this.barras });
-      console.log('passou aqui');
+    if (this.linhaSucesso && this.barraSucesso) { // verifica se os arquivos de linha e de barra têm sucesso
+      this.arquivosCarregados.emit({ linhas: this.arquivoLinha, barras: this.arquivoBarra }); // envia o evento com os arquivos
     }
   }
 
-  getData(readFile, tipo) {
-    const reader = new FileReader();
+  getData(arquivoLido, tipo) {
+    const fileReader = new FileReader();
 
     // Read file into memory as UTF-8
-    reader.readAsText(readFile, 'UTF-8');
+    fileReader.readAsText(arquivoLido, 'UTF-8');
 
     // Handle progress, success, and errors
-    reader.onprogress = this.updateProgress;
-    // reader.onload = this.loaded;
-    reader.onerror = this.errorHandler;
+    fileReader.onprogress = this.updateProgress;
+    // fileReader.onload = this.loaded;
+    fileReader.onerror = this.errorHandler;
 
+
+    // instância de si próprio.
     const self = this;
 
-    reader.onload = function (evt) {
+    fileReader.onload = function (evt) {
       if (tipo === 'linha') {
+        self.arquivoLinha = [];
+
         const dados = FluxoArquivosComponent.TextoParaLista(evt.target['result'], 7);
         self.linhas = new Array();
         dados.forEach((dado, index) => {
+          self.arquivoLinha.push(dado);
           const linha = new Linha();
           if (index > 1) {
             linha.novaLinha(dado);
@@ -74,10 +89,13 @@ export class FluxoArquivosComponent implements OnInit {
         });
         self.linhaSucesso = true;
       } else if (tipo === 'barra') {
+        self.arquivoBarra = [];
+
         const dados = FluxoArquivosComponent.TextoParaLista(evt.target['result'], 14);
 
         self.barras = new Array();
         dados.forEach((dado, index) => {
+          self.arquivoBarra.push(dado);
           const barra = new Barra();
           if (index > 1) {
             barra.novaBarra(dado);
@@ -103,7 +121,6 @@ export class FluxoArquivosComponent implements OnInit {
     }
   }
 
-
   errorHandler(evt) {
     console.log(evt);
     if (evt.target.error.name === 'NotReadableError') {
@@ -112,10 +129,8 @@ export class FluxoArquivosComponent implements OnInit {
   }
 
   lerArquivo(idArquivoAlterado) {
-    console.log(idArquivoAlterado);
 
-    // https://www.w3.org/TR/FileAPI/#ref-for-dfn-filereader-1
-
+    // https://www.w3.org/TR/FileAPI/#ref-for-dfn-filefileReader-1
     const files = (<HTMLInputElement>document.getElementById(idArquivoAlterado)).files;
     if (files.length > 0) {
       const file = files[0];
