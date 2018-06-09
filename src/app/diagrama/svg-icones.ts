94b23dd48cbf6fab214f4ecfe3de91d8b87e3feb
Andrei Bastos <andreibastos@outlook.com>
2018-06-09 13:02:14 -0300

    adicionado tipagem aos parâmetros de entrada

--- src/app/diagrama/svg-icones.ts
+++ src/app/diagrama/svg-icones.ts
@@ -8,7 +8,7 @@ export class SVGIcone {
 
     }
 
-    static createBus(ID, type) {
+    static createBus(ID: string, type: string) {
         const bus = SVG(ID).size(100, 100);
         const group = bus.group();
         if (type === 'PV' || type === 'VT') {
@@ -26,7 +26,7 @@ export class SVGIcone {
             const line_horizontal = bus.line(20, 50, 95, 50).stroke({ width: 2 }).stroke('#000');
             const line_vertical = bus.line(95, 10, 95, 90).stroke({ width: 5 }).stroke('#000');
             const triangule = bus.path('m25,60l10,-25l10,25l-10,0l-10,0z')
-            .rotate(-90, 25, 60);
+                .rotate(-90, 25, 60);
             group.add(line_horizontal);
             group.add(line_vertical);
             group.add(triangule);
