4ce46f23036c5c64c159b357e6d6df6804935359
Andrei Bastos <andreibastos@outlook.com>
2018-06-10 21:44:57 -0300

    adicionando múltiplos nós

--- src/app/models/diagramaSEP.ts
+++ src/app/models/diagramaSEP.ts
@@ -12,7 +12,7 @@ export class DiagramaSEP {
     private _links = [];
 
     private _dict_nodes: Map<number, IComponente> = new Map();
-    private _container: d3.Selection<d3.BaseType, {}, HTMLElement, any>;
+    private _container: SVG.Doc;
 
     private _dict_svg_elements: Map<string, SVG.Element> = new Map();
     count = 0;
@@ -24,17 +24,10 @@ export class DiagramaSEP {
         this._count_components[EnumBar.VT] = 0;
         this._count_components[EnumBar.PQ] = 0;
         this._count_components[EnumBar.Slack] = 0;
-        this._container = d3.select(div)
-            .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .append('g');
-        console.log(this._container);
 
-        const draw = SVG(div).size(width, height);
-        const circle = draw.circle(100).move(width / 2, height / 2);
-        this._dict_svg_elements.set(circle.id(), circle);
-        circle.addClass('component-simple');
+        this._container = SVG(div)
+            .size(width, height);
+
         this.initInteract();
     }
 
@@ -43,11 +36,17 @@ export class DiagramaSEP {
         interact('.component-simple')
             .draggable({
                 inertia: true, // enable inertial throwing
-                autoScroll: true // enable autoScroll
+                autoScroll: true, // enable autoScroll
+                // keep the element within the area of draw_inside
+                restrict: {
+                    restriction: document.getElementById(this._container.id()),
+                    endOnly: true,
+                    elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+                }
             })
             .on('dragstart', function (event) {
-                console.log(event.target, 'dragstart');
-                console.log(self._dict_svg_elements);
+                // console.log(event.target, 'dragstart');
+                // console.log(self._dict_svg_elements);
 
             })
             .on('dragmove', function (event) {
@@ -58,10 +57,12 @@ export class DiagramaSEP {
                     .dy(event.dy);
             })
             .on('dragend', function (event) {
-                console.log(event.target, 'dragend');
-                console.log(self._dict_svg_elements);
+                // console.log(event.target, 'dragend');
+                // console.log(self._dict_svg_elements);
 
             });
+
+
     }
 
 
@@ -76,16 +77,43 @@ export class DiagramaSEP {
         newComponent.name += ' ' + this._count_components[newComponent.type];
         this._dict_nodes.set(newComponent.id, newComponent);
         this.count++;
-
-        this._container.append('circle')
-            .attr('cx', 50)
-            .attr('cy', 50)
-            .attr('r', 50)
-            .style('fill', '#ff0');
-
+        const node = this.createNode(name)
+            .id(newComponent.name)
+            .move(this._container.width() / 2, this._container.height() / 2);
+        this._dict_svg_elements.set(node.id(), node);
+        node.addClass('component-simple');
         console.log(this.getNodes());
 
         return newComponent.id;
+    }
+
+    createNode(name: string): SVG.Element {
+        const node = this._container;
+        const group = node.group();
+        if (name === 'PV' || name === 'VT') {
+            const circle = node.circle(50).move(2, 25).fill('#FFF').stroke({ width: 2 }).stroke('#000');
+            const line_horizontal = node.line(52, 50, 95, 50).stroke({ width: 2 }).stroke('#000');
+            const line_vertical = node.line(95, 10, 95, 90).stroke({ width: 5 }).stroke('#000');
+            const text = node.text(name === 'PV' ? '~' : '∞').font({ size: 50, family: 'Times New Roman' }).move(10, 20);
+            group.add(circle);
+            group.add(line_horizontal);
+            group.add(line_vertical);
+            group.add(text);
+
+        } else if (name === 'PQ') {
+            const line_horizontal = node.line(20, 50, 95, 50).stroke({ width: 2 }).stroke('#000');
+            const line_vertical = node.line(95, 10, 95, 90).stroke({ width: 5 }).stroke('#000');
+            const triangule = node.path('m25,60l10,-25l10,25l-10,0l-10,0z')
+                .rotate(-90, 25, 60);
+            group.add(line_horizontal);
+            group.add(line_vertical);
+            group.add(triangule);
+            // group.rotate(180);
+            console.log(group);
+
+        }
+        return group;
+
     }
 
     getNewBus(name: string) {
