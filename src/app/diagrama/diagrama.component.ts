2305fefa4bfeeb93e51d33387899940821b67de9
Andrei Bastos <andreibastos@outlook.com>
2018-06-18 23:45:42 -0300

    adicionado formulario de editar barra

--- src/app/diagrama/diagrama.component.ts
+++ src/app/diagrama/diagrama.component.ts
@@ -198,7 +198,7 @@ export class DiagramaComponent implements OnInit {
 
   addSelect() {
     if (this.selections.length() === 1) {
-      this.selected = this.selections.get(0).data('data');
+      this.selected = this.selections.get(0).data('barra');
     } else {
       this.selected = null;
     }
@@ -355,6 +355,46 @@ export class DiagramaComponent implements OnInit {
       })
       .on('dragend', function (event) {
       });
+
+    interact('.component-fixed')
+      .draggable({
+        inertia: true, // enable inertial throwing
+        autoScroll: true, // enable autoScroll
+        // keep the element within the area of draw_inside
+        restrict: {
+          restriction: document.getElementsByClassName('sidebar').item(0),
+          endOnly: true,
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        }
+      })
+      .on('dragstart', function (event) {
+        event.interaction.x = parseInt(event.target.getAttribute('data-x'), 10) || 0;
+        event.interaction.y = parseInt(event.target.getAttribute('data-y'), 10) || 0;
+      })
+      .on('dragmove', function (event) {
+        console.log(event.currentTarget);
+
+        const target = event.target,
+          // keep the dragged position in the data-x/data-y attributes
+          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+        // translate the element
+        target.style.webkitTransform =
+          target.style.transform =
+          'translate(' + x + 'px, ' + y + 'px)';
+
+        // update the posiion attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+
+      })
+      .on('dragend', function (event) { });
+
+
+    interact('.dropzone-diagram').dropzone({
+      overlap: 0.1
+    });
   }
 
   add(name: string) {
