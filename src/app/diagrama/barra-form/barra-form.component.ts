1a2ebd7de5cc79c20d88de8133806a98f20deecd
Andrei Bastos <andreibastos@outlook.com>
2018-06-24 19:49:57 -0300

    modificação do mapeamento das barras e das linhas

--- src/app/diagrama/barra-form/barra-form.component.ts
+++ src/app/diagrama/barra-form/barra-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Barra } from '../models/barra';
 
 @Component({
   selector: 'app-barra-form',
@@ -8,7 +9,11 @@ import { FormGroup } from '@angular/forms';
 })
 export class BarraFormComponent implements OnInit {
 
+
+  @Input() barras: Array<Barra>;
+
   formulario: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
